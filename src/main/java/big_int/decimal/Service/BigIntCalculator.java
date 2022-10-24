b00de4bf1f64b72a16ac9387ca14449c682fd4a5
dev-msj <msj944@naver.com>
2022-10-24 17:24:03 +0900

    refactor : 클래스 변수를 plus의 지역 변수로 변경

--- src/main/java/big_int/decimal/Service/BigIntCalculator.java
+++ src/main/java/big_int/decimal/Service/BigIntCalculator.java
@@ -3,12 +3,13 @@ package big_int.decimal.Service;
 import big_int.decimal.Model.BigInt;
 
 public class BigIntCalculator {
-    private final static StringBuilder resultDecimalString = new StringBuilder();
-    private static int upperNumber = 0;
 
     public static BigInt plus(BigInt targetA, BigInt targetB) {
+        StringBuilder resultDecimalString = new StringBuilder();
+        int upperNumber = 0;
         int lengthA = targetA.getDecimalString().length() - 1;
         int lengthB = targetB.getDecimalString().length() - 1;
+
         while (lengthA >= 0 || lengthB >= 0) {
             int intA = lengthA < 0 ? 0 : Character.getNumericValue(targetA.getDecimalString().charAt(lengthA));
             int intB = lengthB < 0 ? 0 : Character.getNumericValue(targetB.getDecimalString().charAt(lengthB));
