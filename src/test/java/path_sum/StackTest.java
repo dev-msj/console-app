02fe761af2bc86181bce5f589907a77e3789a098
dev-msj <msj944@naver.com>
2022-12-01 16:27:38 +0900

    test : stack class 테스트 코드 추가

---
+++ src/test/java/path_sum/StackTest.java
@@ -0,0 +1,35 @@
+package path_sum;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class StackTest {
+    Stack stack = new Stack();
+    Integer[] data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+    @BeforeEach
+    public void setup() {
+        for (int d : data)
+            stack.push(d);
+    }
+
+    @Test
+    void pushTest() {
+        assertEquals(data.length - 1, stack.getPosition());
+    }
+
+    @Test
+    void popTest() {
+        for (int i = data.length - 1; i >= 0; i--)
+            assertEquals(data[i], stack.pop());
+
+        assertThrows(EmptyStackException.class, () -> {
+            stack.pop();
+        });
+    }
+}
